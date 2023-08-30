2262c92fa2d2dd75e6c86deffac227185d4dfbf3
amisoni70 <amisoni7014@gmail.com>
2023-08-30 13:36:49 -0700

    Task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes (frees) an entire binary tree using recursion
+ * @tree: tree that needs to be deleted
+**/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	if (tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+	}
+	free(tree);
+}	
